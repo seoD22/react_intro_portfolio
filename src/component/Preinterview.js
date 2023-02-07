0598c78827657edeed3927f581352f5183b95f3a
seoD22 <cobbt258@naver.com>
2023-02-07 18:04:28 +0900

    preinterview제작 중_230207

--- src/component/Preinterview.js
+++ src/component/Preinterview.js
@@ -1,9 +1,24 @@
 import React from 'react';
+// import Portfolioin from './Preinterviewin'
 
 function Preinterview(props) {
   return (
     <div id='preinterview' className='container_left'>
-      
+      <h2>Preinterview</h2>
+      <div className='pre_button_pc'>
+        <div className='pre_button_1 ms-4 me-4 click'>
+          첫번째 질문~~~~입니다.
+        </div>
+        <div className='pre_button_2 ms-4 me-4'>
+          두번째 질문~~~~입니다.
+        </div>
+        <div className='pre_button_3 ms-4 me-4'>
+          세번째 질문~~~~입니다.
+        </div>
+        <div className='pre_button_4 ms-4 me-4'>
+          네번째 질문~~~~입니다.
+        </div>
+      </div>
     </div>
   );
 }
