4523ec18484be2b13f02a471141b6520c24b27b2
seoD22 <cobbt258@naver.com>
2023-02-08 15:09:14 +0900

    preinterview 모바일버전 제작 완_230208

---
+++ src/component/Contact.js
@@ -0,0 +1,104 @@
+import * as React from 'react';
+import Select from "react-select";
+import { useForm, Controller } from "react-hook-form";
+import { Checkbox, FormControlLabel } from "@material-ui/core";
+
+function Contact(props) {
+
+  const styleValue = ['html_css', 'jQuery', 'ECMA6', 'react', 'node', 'php'];
+
+  const { register, handleSubmit, control, watch, formState: { errors } } = useForm({
+    defaultValues: {
+      firstName: '',
+      select: {}
+    }
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+  console.log(watch("example")); // watch input value by passing the name of it
+
+  return (
+    <section className='container'>
+      <div className="row">
+        <div className='col-4'>
+          여기에 사진넣을까????<br />
+          아님 뭐넣을까?????<br />
+          당신이 나한테 연락을 해야하는 이유를 넣는다...
+        </div>
+        <div className='col'>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <ul>
+              <li>
+                <span>회사명</span>
+                <input {...register("company", {
+                  required: true,
+                  message: "회사명은 필수 입력입니다.",
+                })} />
+                {errors.company && <p role="alert">{errors.company.message}</p>}
+              </li>
+              <li>
+                <span>이메일</span><input {...register("email", {
+                  required: true,
+                  pattern: {
+                    value: /\S+@\S+\.\S+/,
+                    message: "이메일 형식에 맞지 않습니다.",
+                  },
+                })} placeholder="test@email.com" />
+                {errors.email && <p role="alert">{errors.email.message}</p>}
+              </li>
+              <li>
+                <span>하나선택</span>
+                <select {...register("gender", { required: true, message: "꼭 하나선택하기" })}>
+                  <option value="female">female</option>
+                  <option value="male">male</option>
+                  <option value="other">other</option>
+                </select>
+                {errors.gender && <p role="alert">{errors.gender.message}</p>}
+              </li>
+              <li>
+                <span>수치넣기</span>
+                <input type="number" {...register("age", { min: 18, max: 99 })} />
+              </li>
+              <li>
+                <Controller
+                  name="select"
+                  control={control}
+                  render={({ field }) => <Select
+                    {...field}
+                    options={[
+                      { value: "chocolate", label: "Chocolate" },
+                      { value: "strawberry", label: "Strawberry" },
+                      { value: "vanilla", label: "Vanilla" }
+                    ]}
+                  />}
+                />
+              </li>
+              <li>
+                {
+                  styleValue.map((item, idx) => {
+                    return (
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            name={'ability' + idx}
+                            value={item}
+                          />
+                        }
+                        label={item}
+                      />
+                    )
+                  })
+                }
+              </li>
+            </ul>
+            <input type="submit" />
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
--- src/component/Preinterview.js
+++ src/component/Preinterview.js
@@ -1,22 +1,59 @@
-import React from 'react';
-// import Portfolioin from './Preinterviewin'
+import {useEffect} from 'react';
 
 function Preinterview(props) {
+  const btn = document.querySelectorAll('.pre_button')
+  useEffect(()=>{
+    btn.forEach(function(el){
+      el.addEventListener('click', function(){
+        el.classList.toggle('click')
+      })
+    })
+  }, [])
   return (
     <div id='preinterview' className='container_left'>
       <h2>Preinterview</h2>
-      <div className='pre_button_pc'>
-        <div className='pre_button_1 ms-4 me-4 click'>
+      <div className='pre_button_mobile'>
+        <div className='pre_button ms-4 me-4 click'>
           첫번째 질문~~~~입니다.
         </div>
-        <div className='pre_button_2 ms-4 me-4'>
+        <div className='pre_answer ms-4 me-4'>
+          답변!
+        </div>
+        <div className='pre_button ms-4 me-4'>
           두번째 질문~~~~입니다.
         </div>
-        <div className='pre_button_3 ms-4 me-4'>
+        <div className='pre_answer ms-4 me-4'>
+          답변!
+        </div>
+        <div className='pre_button ms-4 me-4'>
           세번째 질문~~~~입니다.
         </div>
-        <div className='pre_button_4 ms-4 me-4'>
+        <div className='pre_answer ms-4 me-4'>
+          답변!
+        </div>
+        <div className='pre_button ms-4 me-4'>
           네번째 질문~~~~입니다.
+        </div>
+        <div className='pre_answer ms-4 me-4'>
+          답변!
+        </div>
+      </div>
+      <div className='pre_pc'>
+        <div className='pre_btns_pc'>
+          <div className='pre_button_pc'>
+            첫번째 질문
+          </div>
+          <div className='pre_button_pc'>
+            두번째 질문
+          </div>
+          <div className='pre_button_pc'>
+            세번째 질문
+          </div>
+          <div className='pre_button_pc'>
+            네번째 질문
+          </div>
+        </div>
+        <div className='pre_answer_pc'>
         </div>
       </div>
     </div>
